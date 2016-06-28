acb9122adda29125a2755c4e9607019e9480ca5d
Sudie Roweton <sudie.roweton@gmail.com>
2016-06-28 12:16:26 -0600

    Adding the initial work setting up the Zipcode class

---
+++ Zipcode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRegistration
+{
+    class Zipcode
+    {
+        private string zip;
+        private string city;
+        private string state;
+        private string createdBy;
+        private string modifiedBy;
+        private DateTime modifiedDate;
+
+        public Zipcode(string zp, string city, string st, string cb, string mb, DateTime md)
+        {
+            Zip = zp;
+            City = city;
+            State = st;
+            CreatedBy = cb;
+            ModifiedBy = mb;
+            ModifiedDate = md;
+
+        }
+
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
