72ed566918e98d6f6b3bb6de83abdcbdfe130125
Sudie Roweton <sudie.roweton@gmail.com>
2016-06-28 12:09:26 -0600

    Adding my initial set up of the Enrollment class

---
+++ Enrollment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRegistration
+{
+    class Enrollment
+    {
+        private string studentID;
+        private string sectionID;
+        private DateTime enrollDate;
+        private string finalGrade;
+        private string createdBy;
+        private string modifiedBy;
+        private DateTime modifiedDate;
+
+        public Enrollment(string sID, string secID, DateTime enrDate, string fgrade, string cb, string mb, DateTime md)
+        {
+            StudentID = sID;
+            SectionID = secID;
+            EnrollDate = enrDate;
+            FinalGrade = fgrade;
+            CreatedBy = cb;
+            ModifiedBy = mb;
+            ModifiedDate = md;
+
+        }
+
+        public string StudentID { get; set; }
+        public string SectionID { get; set; }
+        public DateTime EnrollDate { get; set; }
+        public string FinalGrade { get; set; }
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
